1f31be945aefcd80a932bfe9802674e0a87a8189
Leon Müller <44465413+metigel94@users.noreply.github.com>
2020-05-04 17:34:01 +0200

    Import old project

---
+++ P8-Museum-Usability/Assets/Scripts/ImageTargetContainer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetContainer : MonoBehaviour
+{
+    [SerializeField] GameObject cauldron;
+    [SerializeField] GameObject sword;
+
+    ObjectTracker objectTracker;
+
+    List<GameObject> imageTargets;
+
+    int localTarget;
+    bool firstRun;
+    void Awake()
+    {
+        imageTargets = new List<GameObject>();
+
+        imageTargets.Add(cauldron);
+        imageTargets.Add(sword);
+
+        ImageTargetController.NumberOfImageTargets = imageTargets.Count;
+
+        localTarget = ImageTargetController.CurrentImageTarget;
+        firstRun = true;
+
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(ActiavteImageTarget);
+    }
+
+  
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ImageTargetController.SwitchToNextImageTarget();
+        }
+
+        if (localTarget != ImageTargetController.CurrentImageTarget)
+        {
+            ActiavteImageTarget();
+            localTarget = ImageTargetController.CurrentImageTarget;
+        }
+    }
+
+    private void ActiavteImageTarget()
+    {
+        if (firstRun)
+        {
+            objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            firstRun = false;
+        }
+        
+        objectTracker.Stop();
+
+        foreach (var item in imageTargets)
+        {
+            item.SetActive(false);
+        }
+
+        imageTargets[ImageTargetController.CurrentImageTarget].SetActive(true);
+        
+        objectTracker.Start();
+    }
+}
